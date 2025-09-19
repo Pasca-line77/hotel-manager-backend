8b9633c0304c5c97cf9f89ce54443303447f97a1
Malick Ndiaye <malick00nd@gmail.com>
2025-09-19 09:54:44 +0000

    preparation pour le deploiment de l'API

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
@@ -9,8 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        react(),
-        
         tailwindcss(),
     ],
 });
